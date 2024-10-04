ee4954783aa3604a0db7dee32b1e31e5c0578449
priscila0621 <pjulieths@uamv.edu.ni>
2024-10-04 01:18:21 -0600

    Form hecho

--- Bubble/Form1.Designer.cs
+++ Bubble/Form1.Designer.cs
@@ -28,13 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbNumbers = new System.Windows.Forms.TextBox();
+            this.btnSort = new System.Windows.Forms.Button();
+            this.lbSortedNumbers = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(203, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingresa los numeros separados por coma:";
+            // 
+            // tbNumbers
+            // 
+            this.tbNumbers.Location = new System.Drawing.Point(33, 48);
+            this.tbNumbers.Name = "tbNumbers";
+            this.tbNumbers.Size = new System.Drawing.Size(86, 20);
+            this.tbNumbers.TabIndex = 1;
+            // 
+            // btnSort
+            // 
+            this.btnSort.Location = new System.Drawing.Point(148, 48);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(85, 20);
+            this.btnSort.TabIndex = 2;
+            this.btnSort.Text = "Ordenar";
+            this.btnSort.UseVisualStyleBackColor = true;
+            this.btnSort.Click += new System.EventHandler(this.btnSort_Click);
+            // 
+            // lbSortedNumbers
+            // 
+            this.lbSortedNumbers.FormattingEnabled = true;
+            this.lbSortedNumbers.Location = new System.Drawing.Point(33, 74);
+            this.lbSortedNumbers.Name = "lbSortedNumbers";
+            this.lbSortedNumbers.Size = new System.Drawing.Size(200, 134);
+            this.lbSortedNumbers.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(270, 231);
+            this.Controls.Add(this.lbSortedNumbers);
+            this.Controls.Add(this.btnSort);
+            this.Controls.Add(this.tbNumbers);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbNumbers;
+        private System.Windows.Forms.Button btnSort;
+        private System.Windows.Forms.ListBox lbSortedNumbers;
     }
 }
 
--- Bubble/Form1.cs
+++ Bubble/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Bubble.Models;
 
 namespace Bubble
 {
@@ -16,5 +17,28 @@ namespace Bubble
         {
             InitializeComponent();
         }
+
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            // Obtener la cadena de números desde el TextBox
+            string input = tbNumbers.Text;
+
+            // Separar los números en un arreglo
+            int[] numbers = Array.ConvertAll(input.Split(','), int.Parse);
+
+            // Crear un objeto de la clase Sorter
+            Sorter sorter = new Sorter();
+
+            // Ordenar los números usando bubble sort
+            sorter.BubbleSort(numbers);
+
+            // Mostrar los números ordenados en el ListBox
+            lbSortedNumbers.Items.Clear();
+            foreach (var num in numbers)
+            {
+                lbSortedNumbers.Items.Add(num);
+            }
+        }
     }
+    
 }
