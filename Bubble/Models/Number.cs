1577e249c479b3d7f6e3f87ce7eb2bf9c10420f7
priscila0621 <pjulieths@uamv.edu.ni>
2024-10-04 01:10:16 -0600

    Clase Number

---
+++ Bubble/Models/Number.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bubble.Models
+{
+    // Estructura que representa un número
+    public struct Number
+    {
+        public int Value;
+
+        public Number(int value)
+        {
+            Value = value;
+        }
+    }
+}
